f546537f92d3245c19eddcce61fba2e76e214cb5
sabbir hossain <mdsabbir477470@gmail.com>
2022-11-02 12:29:01 +0600

    done

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// meddle Ware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ok9cief.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('geniusCar').collection('services');
+
+        // all router paba...
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // specific 1 id for...
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+      
+    }
+    finally {
+      
+    }
+  }
+  run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Genies car is running')
+});
+
+app.listen(port, () => {
+    console.log(`Genius Car Server Running on ${port}`);
+});
